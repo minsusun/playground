6646d279b2497d02310571c5e2cb24dd4880ea62
minsusun <poodding397@gmail.com>
2024-07-02 00:14:16 +0900

    feat(auto-git-gc): dump

---
+++ shellscript/auto-git-gc/README.md
@@ -0,0 +1,1 @@
+do `/bin/sh ./script.sh`
---
+++ shellscript/auto-git-gc/script.sh
@@ -0,0 +1,24 @@
+# This script is for auto git gc-ing the _argocd-repo
+# This script tries to limit the total size of the repo in the argocd repo server
+# due to unpacked loos objects in git repository
+
+mkdir -p /tmp/_argocd-repo
+while :
+do
+    chmod +rx /tmp/_argocd-repo
+    if [ $(ls /tmp/_argocd-repo -l | grep '^d' | awk '{print $9}') ]; then
+    chmod +rx /tmp/_argocd-repo/*
+    cd /tmp/_argocd-repo/$(ls /tmp/_argocd-repo -l | grep '^d' | awk '{print $9}');
+    before=$(du /tmp/_argocd-repo -h --max-depth=1 | head -n 1 | awk '{print $1}');
+    git gc;
+    after=$(du /tmp/_argocd-repo -h --max-depth=1 | head -n 1 | awk '{print $1}');
+    if [ "$before" = "$after" ]; then
+        echo $(date) "| git gc with no change";
+    else
+        echo $(date) "| git gc with change" $before "->" $after;
+    fi
+    else 
+    echo "argocd repo directory not found";
+    fi
+    sleep 30s
+done
