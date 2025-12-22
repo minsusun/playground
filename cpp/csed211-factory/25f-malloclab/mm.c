6e2b26eb0d4069f6025a08181780fdd31a7ecd8b
Minsu Sun <20790969+minsusun@users.noreply.github.com>
2025-12-22 19:15:46 +0900

    feat(cpp/csed211): format

--- cpp/csed211-factory/25f-malloclab/mm.c
+++ cpp/csed211-factory/25f-malloclab/mm.c
@@ -1,6 +1,6 @@
 /*
  * mm-naive.c - The fastest, least memory-efficient malloc package.
- * 
+ *
  * In this naive approach, a block is allocated by simply incrementing
  * the brk pointer.  A block is pure payload. There are no headers or
  * footers.  Blocks are never coalesced or reused. Realloc is
@@ -22,18 +22,17 @@
 #define ALIGNMENT 8
 
 /* rounds up to the nearest multiple of ALIGNMENT */
-#define ALIGN(size) (((size) + (ALIGNMENT-1)) & ~0x7)
-
+#define ALIGN(size) (((size) + (ALIGNMENT - 1)) & ~0x7)
 
 #define SIZE_T_SIZE (ALIGN(sizeof(size_t)))
 
 /* Imported from CSAPP:3E */
 /* Basic constants and macros */
-#define WSIZE 4 /* Word and header/footer size (bytes) */
-#define DSIZE 8 /* Double word size (bytes) */
+#define WSIZE 4             /* Word and header/footer size (bytes) */
+#define DSIZE 8             /* Double word size (bytes) */
 #define CHUNKSIZE (1 << 12) /* Extend heap by this amount (bytes) */
 
-#define MAX(x, y) ((x) > (y)? (x) : (y))
+#define MAX(x, y) ((x) > (y) ? (x) : (y))
 
 /* Pack a size and allocated bit into a word */
 #define PACK(size, alloc) ((size) | (alloc))
@@ -55,8 +54,8 @@
 #define NEXT_BLKP(bp) ((void *)(bp) + GET_SIZE(HDRP(bp)))
 #define PREV_BLKP(bp) ((void *)(bp) - GET_SIZE(HDRP(bp) - WSIZE))
 
-#define NEXT_FREE_BLOCK(bp) (*(void**)bp)
-#define PREV_FREE_BLOCK(bp) (*(void**)(bp + WSIZE))
+#define NEXT_FREE_BLOCK(bp) (*(void **)bp)
+#define PREV_FREE_BLOCK(bp) (*(void **)(bp + WSIZE))
 
 static void *heap = NULL;
 static void *free_list = NULL;
@@ -73,14 +72,14 @@ static void list_remove(void *bp);
 
 // #define DEBUG
 
-/* 
+/*
  * mm_init - initialize the malloc package.
  */
 int mm_init(void)
 {
-    if ((heap = mem_sbrk(8 * WSIZE)) == (void*)-1)
+    if ((heap = mem_sbrk(8 * WSIZE)) == (void *)-1)
         return -1;
-    
+
     PUT(heap + (0 * WSIZE), PACK(0, 1));
     PUT(heap + (1 * WSIZE), PACK(6 * WSIZE, 0));
     PUT(heap + (2 * WSIZE), PACK(0, 0));
@@ -94,13 +93,13 @@ int mm_init(void)
     heap += (2 * WSIZE);
 #ifdef DEBUG
     printf("MM_INIT::INFO: heap initialized at %p\n", heap);
-    if(mm_check() == 0)
+    if (mm_check() == 0)
         printf("MM_CHECK::ERROR: mm_check failed in mm_init. Heap is currently inconsistent.\n");
 #endif
     return 0;
 }
 
-/* 
+/*
  * mm_malloc - Allocate a block by incrementing the brk pointer.
  *     Always allocate a block whose size is a multiple of the alignment.
  */
@@ -117,7 +116,7 @@ void *mm_malloc(size_t size)
     }
     place(bp, asize);
 #ifdef DEBUG
-    if(mm_check() == 0)
+    if (mm_check() == 0)
         printf("MM_CHECK::ERROR: mm_check failed in mm_malloc. Heap is currently inconsistent.\n");
 #endif
     return bp;
@@ -135,7 +134,7 @@ void mm_free(void *ptr)
     PUT(FTRP(ptr), PACK(size, 0));
     coalesce(ptr);
 #ifdef DEBUG
-    if(mm_check() == 0)
+    if (mm_check() == 0)
         printf("MM_CHECK::ERROR: mm_check failed in mm_free. Heap is currently inconsistent.\n");
 #endif
 }
@@ -152,7 +151,7 @@ void *mm_realloc(void *ptr, size_t size)
         mm_free(ptr);
         return NULL;
     }
-    
+
     size_t current_size = GET_SIZE(HDRP(ptr)), requested_size = MAX(ALIGN(size) + DSIZE, 4 * WSIZE);
 
     if (current_size >= requested_size)
@@ -185,58 +184,74 @@ void *mm_realloc(void *ptr, size_t size)
     }
 }
 
-static int mm_check(void) {
+static int mm_check(void)
+{
     void *cursor;
     // Is every block in the free list marked as free?
-    for (cursor = free_list; cursor != NULL && GET_ALLOC(HDRP(cursor)) == 0; cursor = NEXT_FREE_BLOCK(cursor)) {
-        if (GET_ALLOC(HDRP(cursor))) {
+    for (cursor = free_list; cursor != NULL && GET_ALLOC(HDRP(cursor)) == 0; cursor = NEXT_FREE_BLOCK(cursor))
+    {
+        if (GET_ALLOC(HDRP(cursor)))
+        {
             printf("MM_CHECK::ERROR: block %p in free list but marked allocated!\n", cursor);
             return 0;
         }
     }
     // Are there any contiguous free blocks that somehow escaped coalescing?
-    for (cursor = free_list; cursor != NULL && GET_ALLOC(HDRP(cursor)) == 0; cursor = NEXT_FREE_BLOCK(cursor)) {
+    for (cursor = free_list; cursor != NULL && GET_ALLOC(HDRP(cursor)) == 0; cursor = NEXT_FREE_BLOCK(cursor))
+    {
         // Check the header and footer of each free block whether it is contiguous with its previous block
         char *prev = PREV_FREE_BLOCK(HDRP(cursor));
-        if (prev != NULL && HDRP(cursor) - FTRP(prev) == DSIZE) {
+        if (prev != NULL && HDRP(cursor) - FTRP(prev) == DSIZE)
+        {
             printf("MM_CHECK::ERROR: block %p missed coalescing!\n", cursor);
             return 0;
         }
     }
     // Is every free block actually in the free list?
-    for (cursor = heap; GET_SIZE(HDRP(cursor)) > 0; cursor = NEXT_BLKP(cursor)) {
-        if (GET_ALLOC(HDRP(cursor)) == 0) {
+    for (cursor = heap; GET_SIZE(HDRP(cursor)) > 0; cursor = NEXT_BLKP(cursor))
+    {
+        if (GET_ALLOC(HDRP(cursor)) == 0)
+        {
             void *bp;
-            for (bp = free_list; bp != NULL && GET_ALLOC(HDRP(bp)) == 0; bp = NEXT_FREE_BLOCK(bp)) {
+            for (bp = free_list; bp != NULL && GET_ALLOC(HDRP(bp)) == 0; bp = NEXT_FREE_BLOCK(bp))
+            {
                 if (bp == cursor)
                     break;
             }
-            if (bp == NULL) {
+            if (bp == NULL)
+            {
                 printf("MM_CHECK::ERROR: free block %p not in free list!\n", cursor);
                 return 0;
             }
         }
     }
     // Do the pointers in the free list point to valid free blocks?
-    for (cursor = free_list; cursor != NULL && GET_ALLOC(HDRP(cursor)) == 0; cursor = NEXT_FREE_BLOCK(cursor)) {
-        if (cursor < mem_heap_lo() || cursor > mem_heap_hi()) {
+    for (cursor = free_list; cursor != NULL && GET_ALLOC(HDRP(cursor)) == 0; cursor = NEXT_FREE_BLOCK(cursor))
+    {
+        if (cursor < mem_heap_lo() || cursor > mem_heap_hi())
+        {
             printf("MM_CHECK::ERROR: free block %p invalid", cursor);
             return 0;
         }
     }
     // Do any allocated blocks overlap?
-    for (cursor = NEXT_BLKP(heap); GET_SIZE(HDRP(cursor)) > 0; cursor = NEXT_BLKP(cursor)) {
-        if (GET_ALLOC(HDRP(cursor))) {
+    for (cursor = NEXT_BLKP(heap); GET_SIZE(HDRP(cursor)) > 0; cursor = NEXT_BLKP(cursor))
+    {
+        if (GET_ALLOC(HDRP(cursor)))
+        {
             void *prev = PREV_BLKP(cursor);
-            if (prev != NULL && GET_ALLOC(HDRP(prev)) && HDRP(cursor) - FTRP(prev) < WSIZE) {
+            if (prev != NULL && GET_ALLOC(HDRP(prev)) && HDRP(cursor) - FTRP(prev) < WSIZE)
+            {
                 printf("MM_CHECK::ERROR: allocated block %p overlaps with allocated block %p!\n", cursor, prev);
                 return 0;
             }
         }
     }
     // Do the pointers in a heap block point to valid heap addresses?
-    for (cursor = heap; GET_SIZE(HDRP(cursor)) > 0; cursor = NEXT_BLKP(cursor)) {
-        if (cursor < mem_heap_lo() || cursor > mem_heap_hi()) {
+    for (cursor = heap; GET_SIZE(HDRP(cursor)) > 0; cursor = NEXT_BLKP(cursor))
+    {
+        if (cursor < mem_heap_lo() || cursor > mem_heap_hi())
+        {
             printf("MM_CHECK::ERROR: block %p outside designated heap space!\n", cursor);
             return 0;
         }
@@ -245,11 +260,11 @@ static int mm_check(void) {
     return 1;
 }
 
-static void* extend_heap(size_t words)
+static void *extend_heap(size_t words)
 {
     void *bp;
     size_t size = ((words % 2) ? words + 1 : words) * WSIZE;
-    if((bp = mem_sbrk(size)) == (void*)-1)
+    if ((bp = mem_sbrk(size)) == (void *)-1)
         return NULL;
 
     PUT(HDRP(bp), PACK(size, 0));
@@ -302,21 +317,24 @@ static void *coalesce(void *bp)
 {
     void *prev_bp = PREV_BLKP(bp), *next_bp = NEXT_BLKP(bp);
     size_t prev_alloc = GET_ALLOC(FTRP(prev_bp)) || prev_bp == bp,
-        next_alloc = GET_ALLOC(HDRP(next_bp)),
-        new_size = GET_SIZE(HDRP(bp));
+           next_alloc = GET_ALLOC(HDRP(next_bp)),
+           new_size = GET_SIZE(HDRP(bp));
 
-    if (!prev_alloc && !next_alloc) {
+    if (!prev_alloc && !next_alloc)
+    {
         new_size += GET_SIZE(HDRP(prev_bp)) + GET_SIZE(HDRP(next_bp));
         list_remove(prev_bp);
         list_remove(next_bp);
         bp = prev_bp;
     }
-    else if (!prev_alloc && next_alloc) {
+    else if (!prev_alloc && next_alloc)
+    {
         new_size += GET_SIZE(HDRP(prev_bp));
         list_remove(prev_bp);
         bp = prev_bp;
     }
-    else if (prev_alloc && !next_alloc) {
+    else if (prev_alloc && !next_alloc)
+    {
         new_size += GET_SIZE(HDRP(next_bp));
         list_remove(next_bp);
     }
@@ -327,7 +345,7 @@ static void *coalesce(void *bp)
     list_insert(bp);
 
 #ifdef DEBUG
-    if(mm_check() == 0)
+    if (mm_check() == 0)
         printf("MM_CHECK::ERROR: mm_check failed in coalesce. Heap is currently inconsistent.\n");
 #endif
     return bp;
@@ -342,7 +360,8 @@ static void list_insert(void *bp)
         free_list = bp;
         return;
     }
-    else {
+    else
+    {
         NEXT_FREE_BLOCK(bp) = free_list;
         PREV_FREE_BLOCK(bp) = NULL;
         PREV_FREE_BLOCK(free_list) = bp;
